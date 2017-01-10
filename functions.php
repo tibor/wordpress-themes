f974ba9a29b4eadff334928c089241c4e3aeb18d
Tibor Martini <tibor@users.noreply.github.com>
2017-01-10 10:38:35 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+add_theme_support('title-tag');
+add_theme_support('post-thumbnails');
+
+function disable_wp_emojicons() {
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+add_filter( 'emoji_svg_url', '__return_false' );
+
+add_filter('wp_theme_editor_filetypes', function ($types) {
+  $types[] = 'scss';
+  $types[] = 'js';
+  return $types;
+});
