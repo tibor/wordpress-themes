342e3dea2d8bc1ff17f89499670da1e684b2fb59
Tibor Martini <tibor@users.noreply.github.com>
2017-01-25 11:55:07 +0100

    CSS für Wordpress Admin-Bar entfernen

--- functions.php
+++ functions.php
@@ -19,3 +19,9 @@ add_filter('wp_theme_editor_filetypes', function ($types) {
   $types[] = 'js';
   return $types;
 });
+
+add_action('get_header', 'my_filter_head');
+
+  function my_filter_head() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+  }
